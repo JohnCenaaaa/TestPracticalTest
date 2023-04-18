06811c5a233e45310f784e9f0be2dee681b3f4ea
Yuhan <yhchan6@gmail.com>
2023-04-18 20:50:24 +0800

    update 2

--- app/src/main/java/com/example/testpracticaltest/MainActivity.kt
+++ app/src/main/java/com/example/testpracticaltest/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
 import org.w3c.dom.Text
@@ -25,7 +27,23 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.adapter = adapter
         }
+        val gradeButton : Button = findViewById(R.id.Submit)
+        gradeButton.setOnClickListener{
+            val score : EditText = findViewById(R.id.Score)
 
+            val result = when{
+                score.text.toString().toDouble() >= 80 -> "A"
+                score.text.toString().toDouble() >= 75 -> "A-"
+                score.text.toString().toDouble() >= 70 -> "B+"
+                score.text.toString().toDouble() >= 65 -> "B"
+                score.text.toString().toDouble() >= 60 -> "B-"
+                score.text.toString().toDouble() >= 55 -> "C+"
+                score.text.toString().toDouble() >= 50 -> "C"
+                else -> "D"
+            }
+            val resultText : TextView = findViewById(R.id.Result)
+            resultText.text = "Result: " + " " + result
+        }
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
